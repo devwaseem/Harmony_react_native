35063e0664a6c87901bd7dccde17bfe713a2213c
unknown <pooja.kiotek@gmail.com>
2021-01-08 00:55:00 +0530

    Icons done

---
+++ src/Screens/home.tsx
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import HarmonyButton,{HButtonProps,HButtonStyle,HButtonState} from '../components/Forms/HButton';
+import HarmonyIcon,{HIconProps,HIconStyle,HIconState} from '../components/Forms/HIcon';
+import { SafeAreaView, StatusBar, View, Alert } from 'react-native';
+
+const HomeScreen= () => {
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <View>
+            <HarmonyButton 
+              type={HButtonStyle.secondary}
+              state={HButtonState.disabled}
+              isLoading={true}
+              title="Button"
+              onPress={() => Alert.alert('Simple Button pressed')}
+            />   
+            <HarmonyIcon
+              type={HIconStyle.primary}
+              state={HIconState.active}
+              title="Icon"
+              onPress={() => Alert.alert('icon pressed')}
+            /> 
+          </View>
+        </SafeAreaView>
+      </>
+    );
+  };
+
+export default HomeScreen;
